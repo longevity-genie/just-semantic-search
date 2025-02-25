d2a1b6fd60320ce73d6c315d06c307dd637492a4
winternewt <isoutthere@gmail.com>
2025-02-26 00:38:43 +0300

    Fixed cuda deps on cpu

--- server/containers/rag-server/build.sh
+++ server/containers/rag-server/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 script_dir=$(dirname "$(readlink -f "$0")")
 docker build -f ${script_dir}/Dockerfile -t ghcr.io/longevity-genie/just-semantic-search/rag-server:local ${script_dir}/../../../
-#podman pull docker-daemon:ghcr.io/longevity-genie/just-semantic-search/rag-server:local
+podman pull docker-daemon:ghcr.io/longevity-genie/just-semantic-search/rag-server:local
